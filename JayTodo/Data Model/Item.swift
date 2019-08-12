617fde1bf58ec14d6d87bdeedb599fd71b76a7f5
John Spina <JohnSpina@johns-mbp-3.lan1>
2019-08-12 09:53:28 -0400

    Convert to MVC design pattern.

---
+++ JayTodo/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  JayTodo
+//
+//  Created by John Spina on 8/12/19.
+//  Copyright © 2019 jspina. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+  
+  var title : String = ""
+  var done : Bool = false
+  
+}
